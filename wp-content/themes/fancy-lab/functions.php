2aa6c604788211cb7c3b89ddb06babfc0825cbf6
Jean-PierrePapin <skouo82@gmail.com>
2020-05-10 13:48:56 +0100

    How to make a simple menu in WP

--- wp-content/themes/fancy-lab/functions.php
+++ wp-content/themes/fancy-lab/functions.php
@@ -11,6 +11,15 @@
  function fancy_lab_scripts() {
      wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/inc/bootstrap.min.js', [ 'jquery' ], '4.4.1', true );
      wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/inc/bootstrap.min.css', [], '4.4.1', 'all' );
-     wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), [], filetime( get_template_directory() . '/style.css' ), 'all' );
+     wp_enqueue_style( 'fancy-lab-style', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
  }
  add_action( 'wp_enqueue_scripts', 'fancy_lab_scripts' );
+
+ function fancy_lab_config() {
+    register_nav_menus(
+        [
+            'fancy_lab_main_menu'   =>  'Fancy Lab Main Menu'
+        ]
+    );
+ }
+ add_action( 'after_setup_theme', 'fancy_lab_config', 0 );
