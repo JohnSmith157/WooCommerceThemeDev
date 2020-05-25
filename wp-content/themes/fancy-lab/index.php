f90fe2ab4444b3c6e5355d293b5834b4b77ff983
Jean-PierrePapin <skouo82@gmail.com>
2020-05-25 20:30:20 +0100

    Modifying the blog posts index template file

--- wp-content/themes/fancy-lab/index.php
+++ wp-content/themes/fancy-lab/index.php
@@ -25,9 +25,32 @@ get_header();
                     while ( have_posts() ) {
                         the_post();
                         ?>
-                            <article>
-                                <h2><?php the_title(); ?></h2>
-                                <div><?php the_content(); ?></div>
+                            <article <?php post_class(); ?>>
+                                <h2>
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>        
+                                </h2>
+                                <div class="post-thumbnail">
+                                    <?php
+
+                                    if( has_post_thumbnail() ) {
+                                        the_post_thumbnail( 'fancy-lab-blog', ['class' => 'img-fluid'] );
+                                    }
+
+                                    ?>
+                                </div>
+                                <div class="meta">
+                                    <p>Published by <?php the_author_posts_link(); ?> on <?php echo get_the_date(); ?>
+                                        <br />
+                                        <?php if( has_category() ) { ?>
+                                            Categories: <span><?php the_category( ' ' ); ?></span>
+                                        <?php } ?>
+                                        <br />
+                                        <?php if( has_tags() ) { ?>
+                                            Tags: <span><?php the_tags( '', ', ' ); ?></span>
+                                        <?php } ?>
+                                    </p>
+                                </div>
+                                <div><?php the_excerpt(); ?></div>
                             </article>
                             <br>
                         <?php
