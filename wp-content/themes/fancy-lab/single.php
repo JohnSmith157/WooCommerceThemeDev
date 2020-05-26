8f12f5a8793d68db075b2522ec35332117de4c86
Jean-PierrePapin <skouo82@gmail.com>
2020-05-26 21:04:42 +0100

    comments form

--- wp-content/themes/fancy-lab/single.php
+++ wp-content/themes/fancy-lab/single.php
@@ -51,6 +51,9 @@ get_header();
                     </article>
 
                     <?php
+                    if( comments_open() || get_comments_number() ) {
+                        comments_template();
+                    }
                 }
 
                 ?>
